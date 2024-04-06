ec30a0381075f5e588f4f65efe27e7a71930fa2a
Jeremy <jhirst8@myune.edu.au>
2024-04-07 09:00:20 +1000

    add buttons for player selection

--- game.js
+++ game.js
@@ -8,34 +8,56 @@ return compChoice;
 }
 
 // console.log(getComputerChoice())
+let playerSelection = "";
+const rockButton = document.querySelector("#rock");
 
-
+rockButton.addEventListener("click", () => {
+    playerSelection = 'rock';
+    playGame();
+})
+const paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", ()=> {
+    playerSelection = 'paper';
+    playGame();
+})
+const scissorButton = document.querySelector("#scissors")
+scissorButton.addEventListener("click", ()=> {
+    playerSelection = 'scissors'
+    playGame();
+})
 // FUNCTION - playGame (playerSelection, computerSelection) - return String that indicates winner of round
 function playGame(){
     let computerSelection = getComputerChoice();
-    const playerSelection = prompt('Rock, Paper, or Scissors? ' )
     const player = playerSelection.toLowerCase();
+    let compScore = 0;
+    let playerScore = 0;
 
     let outcome = ''
     if (player === 'rock' && computerSelection === 'paper'){
         outcome = 'Lose, Paper beats Rock!'
+        compScore += 1
     }
     else if(player === 'rock' && computerSelection ==='scissors'){
         outcome = 'Win, Rock beats Scissors!'
+        playerScore += 1;
     }
 
     else if(player === 'paper' && computerSelection=== 'rock'){
         outcome = 'Win, Paper beats rock!'
+        playerScore += 1;
     }
 
     else if(player === 'paper' && computerSelection === 'scissors'){
         outcome = 'Lose, Scissors beats paper!'
+        compScore += 1
     }
     else if(player === 'scissors' && computerSelection === 'rock'){
         outcome = 'Lose, Rock beats Scissors!'
+        compScore += 1
     }
     else if(player === 'scissors' && computerSelection === 'paper'){
         outcome = 'Win, Scissors beats paper!'
+        playerScore += 1;
     }
 
     else{
@@ -46,35 +68,35 @@ function playGame(){
 
 }
 
-console.log(game());
+// console.log(game());
 
 //FUNCTION  - game(playGame()) - play 5 rounds, keep score, and reports winner at end
 
-function game(){
-    let playerScore = 0
-    let compScore = 0
-    for (i = 0; i < 5; i++){
-        let score = playGame()
-        if (score.startsWith('Win')){
-            playerScore +=1
-        }
-        else if (score.startsWith('Lose')){
-            compScore +=1
-        }
-        console.log('Player: ' + playerScore)
-        console.log('Computer: '+ compScore)
+// function game(){
+//     let playerScore = 0
+//     let compScore = 0
+//     for (i = 0; i < 5; i++){
+//         let score = playGame()
+//         if (score.startsWith('Win')){
+//             playerScore +=1
+//         }
+//         else if (score.startsWith('Lose')){
+//             compScore +=1
+//         }
+//         console.log('Player: ' + playerScore)
+//         console.log('Computer: '+ compScore)
 
-    }
+//     }
 
-    if (playerScore > compScore){
-        console.log('Congratulation, you won!')
-    }
+//     if (playerScore > compScore){
+//         console.log('Congratulation, you won!')
+//     }
 
-    else if (compScore > playerScore){
-        console.log('Commiserations, you Lost!')
-    }
+//     else if (compScore > playerScore){
+//         console.log('Commiserations, you Lost!')
+//     }
 
-    else{
-        console.log('Its a Draw!')
-    }
-}
+//     else{
+//         console.log('Its a Draw!')
+//     }
+// }
